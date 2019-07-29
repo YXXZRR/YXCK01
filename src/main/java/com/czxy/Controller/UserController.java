b541d01fcb2b3872739b27e0f913e6a343d7bd3c
hrc <370031108@qq.com>
2019-07-29 08:48:39 +0800

    添加test工程至git

---
+++ src/main/java/com/czxy/Controller/UserController.java
@@ -0,0 +1,11 @@
+package com.czxy.Controller;
+
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("哦嚯嚯嚯");
+        System.out.println("哦嚯嚯嚯");
+        System.out.println("哦嚯嚯嚯");
+        System.out.println("哦嚯嚯嚯");
+        System.out.println("哦嚯嚯嚯");
+    }
+}
